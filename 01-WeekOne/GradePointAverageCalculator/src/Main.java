3ff28b04cc2bc20bad35192dbb7f1c2313be4732
semih turan <semituran@gmail.com>
2024-02-07 13:56:59 +0300

    remove unnecessary comments

--- 01-WeekOne/GradePointAverageCalculator/src/Main.java
+++ 01-WeekOne/GradePointAverageCalculator/src/Main.java
@@ -1,5 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 /**
  * @author Semih Turan
  * @since february 2024
