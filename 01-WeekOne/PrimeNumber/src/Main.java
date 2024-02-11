6883897f44ee5e7acf51b9b70dcc204726503882
Semih Turan <semituran@gmail.com>
2024-02-12 00:13:13 +0300

    add prime number files

---
+++ 01-WeekOne/PrimeNumber/src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * @author Semih Turan
+ * @since february 2024
+ */
+public class Main {
+    public static void main(String[] args) {
+        for(int i=2; i<=100; i++){
+            boolean isPrime= true;
+            for(int j=2; j<i;j++){
+                if(i%j==0){
+                    isPrime= false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.print(i +" ");
+            }
+        }
+    }
+}
