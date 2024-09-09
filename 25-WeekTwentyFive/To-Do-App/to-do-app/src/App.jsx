7cd80abdae4731efe79ccb27da06389729600cc3
semih turan <semituran@gmail.com>
2024-09-09 10:19:10 +0300

    add note-app files

---
+++ 25-WeekTwentyFive/To-Do-App/to-do-app/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Header from './components/Header'
+import Notes from './components/Notes'
+
+
+
+function App() {
+  
+
+  return (
+    <div className='main'>
+      <Header/>
+      <Notes/>
+    </div>
+  )
+}
+
+export default App
